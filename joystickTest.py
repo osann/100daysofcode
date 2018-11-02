8201ee922e3bde90e768892f845898f6ad4e928f
Jack <44524569+osann@users.noreply.github.com>
2018-11-02 16:41:25 +1030

    Update - Added LED
    Second day of 100 days of code, I spent an hour trying to get the LED to light up, hit a few snags but I think I got it. This is still not finished.

--- joystickTest.py
+++ joystickTest.py
@@ -4,7 +4,11 @@
 # The goal of this class is to control an LED on the Sense HAT module for the
 # Raspberry Pi.
 # This class simply waits for events from the Sense HAT joystick, then increases
-# or decreases a coordinate for the LED matrix on the HAT.
+# or decreases a coordinate for the LED matrix on the HAT. 
+# Known bugs to fix:
+# - When held, continues being held **FIXED**
+# Left to add:
+# - Ways to make the class implementable to other programs
 # ==========================================================================================
 
 from sense_hat import SenseHat
@@ -13,10 +17,13 @@ from time import sleep
 sense = SenseHat()
 class JoystickLed(object):
     currentPos = [1, 1] # Coordinates
+    led_colour = (255, 255, 255) # Sets the colour of the led highlight, currently white
+    background_colour = (0, 0, 0) # Sets the colour of the background, currently nothing
+    exit = False # Boolean to handle exit
 
     # Initiate, currently does nothing
     def __init__(self):
-        pass
+        sense.set_pixel(self.currentPos[0], self.currentPos[1], self.led_colour)
 
     # Represent, returns led position
     def __repr__(self):
@@ -35,34 +42,30 @@ class JoystickLed(object):
         if self.currentPos[1] < 7:
             self.currentPos[1] += 1
             print(self.currentPos)
-        else:
-            print("Hit bottom") # Remove this before final version
     def led_up(self):
         if self.currentPos[1] > 0:
             self.currentPos[1] -= 1
             print(self.currentPos)
-        else:
-            print("Hit top") # Remove this before final version
     def led_right(self):
         if self.currentPos[0] < 7:
             self.currentPos[0] += 1
             print(self.currentPos)
-        else:
-            print("Hit right side") # Remove this before final version
     def led_left(self):
         if self.currentPos[0] > 0:
             self.currentPos[0] -= 1
             print(self.currentPos)
-        else:
-            print("Hit left side") # Remove this before final version
     # End of LED move functions
     # ==========================================================================================
     
     # Joystick move function
     # Work in conjunction with above functions
     def move(self):
+        tempPos = self.currentPos # Stores last LED coordinates
+        temp_colour = self.background_colour # Stores the current background colour
         event = sense.stick.wait_for_event()
+
         if (event.action == "pressed" or event.action == "held") and event.action != "released":
+            sense.set_pixel(tempPos[0], tempPos[1], temp_colour)
             if event.direction == "up":
                 self.led_up()
             if event.direction == "down":
@@ -71,9 +74,13 @@ class JoystickLed(object):
                 self.led_right()
             if event.direction == "left":
                 self.led_left()
+            if event.direction == "middle":
+                self.exit = True
             print("Went %s" % event.direction)
-            sleep(0.3)
-
+        sense.set_pixel(self.currentPos[0], self.currentPos[1], self.led_colour)
+# ===================
+# End of class
 test = JoystickLed()
-for i in range(0, 20):
+while test.exit != True:
     test.move()
+sense.clear()
